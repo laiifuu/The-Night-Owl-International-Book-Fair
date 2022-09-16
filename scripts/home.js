5ce4e79920a8c2e99ddaa71e00bf2b3593484953
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-16 01:52:21 +0100

    Adjust the urls for the auuthors' images

--- scripts/home.js
+++ scripts/home.js
@@ -1,38 +1,38 @@
 let authorsList = [
     {
       name: "Thomas Harris",
-      photoUrl: "../images/Thomas-Harris.jpg",
+      photoUrl: "images/Thomas-Harris.jpg",
       genre: ["Crime", "Horror", "Suspense"],
       description:
         "He is an American author best known for a series of suspense novels about his most famous character, Hannibal Lecter. The majority of his works have been adapted into films and television, the most notable being The Silence of the Lambs.",
     },
     {
       name: "Stephen King",
-      photoUrl: "../images/Stephen-King.jpg",
+      photoUrl: "images/Stephen-King.jpg",
       genre: ["Horror","Supernatural", "Suspense", "Crime", "Fantasy"],
       description: "Described as the King of Horror, he is an American author with 64 published novels which most of them were adapted into films and series and over 200 short stories.",
     },
     {
       name: "Elif Shafak",
-      photoUrl: "../images/Elif-Shafak.jpg",
+      photoUrl: "images/Elif-Shafak.jpg",
       genre: ["Literary fiction"],
       description: "She is a turkish-british author who writes in both english and turkish. Described by the Financial Times as 'Turkey's leading female novelist', she has 19",
     },
     {
       name: "Haruki Murakami",
-      photoUrl: "../images/Haruki-Murakami.jpg",
+      photoUrl: "images/Haruki-Murakami.jpg",
       genre: ["Fiction", "Surrealism", "Postmodernism", "Bildungsroman"],
       description: "He is a Japaanese writer whose novels, essays, and short stories have been bestsellers in Japan as well as internationally. With his work translated into 50 languages, he has sold millions of copies outside Japan.",
     },
     {
       name: "Colleen Hoover",
-      photoUrl: "../images/Colleen-Hoover.jpg",
+      photoUrl: "images/Colleen-Hoover.jpg",
       genre: ["Young adult fiction", "New-adult fiction", "Romance", "Thriller"],
       description: "She is an American author best known for her 2016 romance novel It Ends with Us. Many of her novels ended up being a New York times best seller. Many of her works were self-published before being picked up by a publishing house.",
     },
     {
       name: "Salley Rooney",
-      photoUrl: "../images/Salley-Rooney.jpg",
+      photoUrl: "images/Salley-Rooney.jpg",
       genre: ["Fiction", "Coming of Age", "Literary", "Psychological"],
       description: " She is an Irish author and screenwriter. She has published three novels. One of them titled 'Normal People' was adapted into a 2020 television series. She is regarded as one of the foremost millennial writers.",
     },
