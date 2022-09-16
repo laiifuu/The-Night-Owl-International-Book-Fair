78a34f1414b426e378753332172044dbf5b936ba
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-16 12:01:40 +0100

    Add burger menu animation

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,29 @@ const menuLinks = [...menu.children];
 const img = button.querySelector("img");
 const body = document.querySelector("body");
 
+
 function removeMenu() {
   menu.classList.remove("toggled-menu");
   button.classList.remove("burger-menu-toggled");
-  img.setAttribute("src", "./images/burger-menu-icon.png");
-  img.setAttribute("data-toggled", "false");
+  button.classList.remove("open");
+  button.setAttribute("data-toggled", "false");
   body.classList.remove("overflow");
 }
 
 button.addEventListener("click", () => {
-  if (img.getAttribute("data-toggled") === "false") {
-    img.src = "./images/x-icon.png";
-    img.setAttribute("data-toggled", "true");
+  if (button.getAttribute("data-toggled") === "false") {
+    button.classList.add('open');
+    button.setAttribute("data-toggled", "true");
   } else {
-    img.src = "./images/burger-menu-icon.png";
-    img.setAttribute("data-toggled", "false");
+    button.classList.remove('open');
+    button.setAttribute("data-toggled", "false");
   }
   button.classList.toggle("burger-menu-toggled");
   menu.classList.toggle("toggled-menu");
   body.classList.toggle("no-scroll");
 });
+
+
 
 menuLinks.forEach((link) => {
   link.addEventListener("click", removeMenu);
